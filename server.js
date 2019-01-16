60b42147f79d2586dc3f2f4a1678d0da45509c0c
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-16 17:05:33 -0600

    server running and get request working

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+const url = '/api/cohorts/';
+
+server.get(url, async(req, res) => {
+    try{
+        const data = await db('cohorts')
+        res.status(200).json(data)
+    } catch(err) {
+        res.status(500).json(`{error: 'This route does not exsi'}`)
+    }
+})
+
+module.exports = server
